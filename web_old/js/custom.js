f00aac1a3a50a66179638406381cce80f1ecd7ec
3874 <59820337+3874@users.noreply.github.com>
2025-08-20 12:56:50 +0900

    2025-08-20

--- web_old/js/custom.js
+++
@@ -1,8 +0,0 @@
-export function CheckSignIn() {
-    const userData = localStorage.getItem('mystaffInfo');
-    if (!userData) {
-        window.location.href = './signin.html';
-        return null; // Added to prevent further execution
-    }
-    return userData;
-}
